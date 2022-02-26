24ef70d5a6d09db0d144ac2cf93091d6e00a6c4b
Cryptojoint <a.hamdy.nig@gmail.com>
2022-02-26 17:50:56 +0200

    React-Zmage bug -replaced React-Zmage with Bootstrap modal -importing Bootstrap style overrides current css

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Zmage from 'react-zmage';
+import { Button, Figure, Image, Modal } from 'react-bootstrap';
 
 let id = 0;
 class Portfolio extends Component {
     render() {
         if (!this.props.data) return null;
 
-        const projects = this.props.data.projects.map(function (projects) {
-            let projectImage = 'images/portfolio/' + projects.image;
-
-            return (
-                <div key={id++} className="columns portfolio-item">
-                    <div className="item-wrap">
-                        <Zmage
-                            alt={projects.title}
-                            src={projectImage}
-                            width="100%"
-                            height="100%"
-                        />
-                        <div style={{ textAlign: 'center' }}>{projects.title}</div>
-                    </div>
-                </div>
-            );
+        const projectsDev = this.props.data.projects.map((project, i) => {
+            return <ProjectModal key={i} project={project} />;
         });
 
         return (
@@ -34,7 +21,7 @@ class Portfolio extends Component {
                             id="portfolio-wrapper"
                             className="bgrid-quarters s-bgrid-thirds cf"
                         >
-                            {projects}
+                            {projectsDev}
                         </div>
                     </div>
                 </div>
@@ -42,5 +29,58 @@ class Portfolio extends Component {
         );
     }
 }
+
+// @bug Zmage
+// when img is in full-screen then closed
+// it leaves a figue div with z-index 1000
+// @fix replaced Zmage with bootstrap modal
+
+const ProjectModal = ({ project }) => {
+    const [show, setShow] = useState(false);
+    const { title, details, image } = project;
+    let projectImage = 'images/portfolio/' + image;
+
+    const handleModalShow = () => setShow(true);
+
+    const handleModalClose = () => setShow(false);
+
+    return (
+        <div key={id++} className="columns portfolio-item">
+            <div className="item-wrap">
+                <Figure>
+                    <Button className="portfolioImg-btn" onClick={handleModalShow}>
+                        <Figure.Image
+                            src={projectImage}
+                            alt={title}
+                            width="100%"
+                            height="100%"
+                        />
+                    </Button>
+                    <Figure.Caption style={{ textAlign: 'center' }}>
+                        {project.title}
+                    </Figure.Caption>
+                    <Modal
+                        show={show}
+                        onHide={handleModalClose}
+                        className="project-modal"
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title>{project.title}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Image
+                                src={projectImage}
+                                alt={title}
+                                width="75%"
+                                height="50%"
+                            />
+                            <p>{details}</p>
+                        </Modal.Body>
+                    </Modal>
+                </Figure>
+            </div>
+        </div>
+    );
+};
 
 export default Portfolio;
