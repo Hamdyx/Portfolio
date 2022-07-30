6cea2e98b6478be9f3d25c2cea5e033f4596b7fa
hamdyx <a.hamdy.nig@gmail.com>
2022-07-30 17:55:34 +0200

    SCSS init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
