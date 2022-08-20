a8ad494a3ffac665b70b302cb568ead44fc685aa
hamdyx <a.hamdy.nig@gmail.com>
2022-08-20 17:56:07 +0200

    refactor footer style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
