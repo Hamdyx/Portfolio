f29054ac5846f3cc562bbc3823dc5ff496b36f1b
hamdyx <a.hamdy.nig@gmail.com>
2022-10-22 20:04:24 +0200

    add AntD

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
 
 import 'animate.css';
+import { Layout } from 'antd';
 
 class App extends Component {
     constructor(props) {
@@ -40,14 +41,14 @@ class App extends Component {
     render() {
         return (
             <React.StrictMode>
-                <div className="App">
+                <Layout className="layout">
                     <Header data={this.state.resumeData.main} />
-                    <About data={this.state.resumeData.main} />
+                    {/* <About data={this.state.resumeData.main} />
                     <Resume data={this.state.resumeData.resume} />
                     <Portfolio data={this.state.resumeData.portfolio} />
                     <Contact data={this.state.resumeData.main} />
-                    <Footer data={this.state.resumeData.main} />
-                </div>
+                    <Footer data={this.state.resumeData.main} /> */}
+                </Layout>
             </React.StrictMode>
         );
     }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,75 +1,79 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ParticlesBg from 'particles-bg';
 import Icons from '../assets/icons.ts';
+import { Layout, Menu } from 'antd';
+const { Header: AntHeader } = Layout;
 
-class Header extends Component {
-    render() {
-        if (!this.props.data) return null;
-        const { name, description, social } = this.props.data;
-        const networks = social.map((network) => {
-            const IconEl = Icons[network.name];
-
-            return (
-                <li key={network.name}>
-                    <a href={network.url} aria-label={network.name}>
-                        {/* <i className={network.className}></i> */}
-                        <IconEl />
-                    </a>
-                </li>
-            );
-        });
-
-        const routes = ['home', 'about', 'resume', 'portfolio', 'contact'];
-
-        const navBtns = routes.map((route) => <NavBtn key={route} route={route} />);
+const Header = ({ data }) => {
+    if (!data) return null;
+    const { name, description, social } = data;
+    const networks = social.map((network) => {
+        const IconEl = Icons[network.name];
 
         return (
-            <header id="home">
-                <ParticlesBg type="cobweb" bg={true} />
-
-                <nav id="nav-wrap">
-                    <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-                        Show navigation
-                    </a>
-                    <a className="mobile-btn" href="#home" title="Hide navigation">
-                        Hide navigation
-                    </a>
-
-                    <ul id="nav" className="nav">
-                        {navBtns}
-                    </ul>
-                </nav>
-
-                <div className="row banner">
-                    <div className="banner-text">
-                        <h1>{name}</h1>
-                        <h2>{description}.</h2>
-                        <hr />
-                        <ul className="social">{networks}</ul>
-                    </div>
-                </div>
-
-                <p className="scrolldown">
-                    <a className="smoothscroll" href="#about" aria-label="scroll-down">
-                        <i className="icon-down-circle"></i>
-                    </a>
-                </p>
-            </header>
+            <li key={network.name}>
+                <a href={network.url} aria-label={network.name}>
+                    <IconEl />
+                </a>
+            </li>
         );
-    }
-}
+    });
 
-const NavBtn = ({ route }) => {
+    const routes = ['home', 'about', 'resume', 'portfolio', 'contact'];
+
+    const navBtns = routes.map((route) => NavBtn({ key: route, route: route }));
+
     return (
-        <li className={route === 'home' ? 'current' : ''}>
+        <AntHeader id="home">
+            <ParticlesBg type="cobweb" bg={true} />
+            <nav id="nav-wrap">
+                {/* <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+                    Show navigation
+                </a>
+                <a className="mobile-btn" href="#home" title="Hide navigation">
+                    Hide navigation
+                </a> */}
+
+                <ul id="nav" className="nav">
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        items={navBtns}
+                    />
+                </ul>
+            </nav>
+
+            <div className="row banner">
+                <div className="banner-text">
+                    <h1>{name}</h1>
+                    <h2>{description}.</h2>
+                    <hr />
+                    <ul className="social">{networks}</ul>
+                </div>
+            </div>
+
+            <p className="scrolldown">
+                <a className="smoothscroll" href="#about" aria-label="scroll-down">
+                    <i className="icon-down-circle"></i>
+                </a>
+            </p>
+        </AntHeader>
+    );
+};
+
+const NavBtn = ({ route, key }) => {
+    return {
+        key,
+        label: (
             <a href={`#${route}`} className="nav-btn smoothscroll">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span> {route}
             </a>
-        </li>
-    );
+        ),
+    };
 };
 
 export default Header;
