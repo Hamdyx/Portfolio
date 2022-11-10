feb02a99cd1fe13860d6ed240ad67cb446b226ab
hamdyx <a.hamdy.nig@gmail.com>
2022-11-11 01:51:34 +0200

    refactor contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,5 @@
+import { Button, Col, Form, Input, Row } from 'antd';
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 import { MdEmail } from 'react-icons/md';
 const Contact = ({ data }) => {
     const { name, phone, address, contactmessage } = data;
@@ -20,95 +20,148 @@ const Contact = ({ data }) => {
                     </p>
                 </div>
             </div>
-            <Container>
-                <Row >
-                    <Col lg={8} className="animate__animated animate__fadeInLeft">
-                        <form
-                            action=""
-                            method="post"
-                            id="contactForm"
-                            name="contactForm"
+            {/* <Container></Container> */}
+            <Row>
+                <Col span={16} className="animate__animated animate__fadeInLeft">
+                    <form
+                        id="contactForm"
+                        name="contactForm"
+                    >
+                        <fieldset>
+                            <div>
+                                <label htmlFor="contactName">
+                                    Name <span className="required">*</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    defaultValue=""
+                                    size="35"
+                                    id="contactName"
+                                    name="contactName"
+                                />
+                            </div>
+
+                            <div>
+                                <label htmlFor="contactEmail">
+
+                                    Email <span className="required">*</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    defaultValue=""
+                                    size="35"
+                                    id="contactEmail"
+                                    name="contactEmail"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="contactSubject">Subject</label>
+                                <input
+                                    type="text"
+                                    defaultValue=""
+                                    size="35"
+                                    id="contactSubject"
+                                    name="contactSubject"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="contactMessage">
+                                    Message <span className="required">*</span>
+                                </label>
+                                <textarea
+                                    cols="50"
+                                    rows="15"
+                                    id="contactMessage"
+                                    name="contactMessage"
+                                ></textarea>
+                            </div>
+                            <div>
+                                <button className="submit">Submit</button>
+                                <span id="image-loader">
+                                    <img alt="" src="images/loader.gif" />
+                                </span>
+                            </div>
+                        </fieldset>
+                    </form>
+                    <Form
+                        // action=""
+                        // method="post"
+                        id="contactForm"
+                        name="contactForm"
+                        // name="basic"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 18 }}
+                        initialValues={{ remember: true }}
+                        // onFinish={onFinish}
+                        // onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item
+                            label="Name"
+                            name="contactName"
+                            rules={[{
+                                required: true
+                                // message: 'Please input your username!' 
+                            }]}
                         >
-                            <fieldset>
-                                <div>
-                                    <label htmlFor="contactName">
-                                        Name <span className="required">*</span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        defaultValue=""
-                                        size="35"
-                                        id="contactName"
-                                        name="contactName"
-                                    />
-                                </div>
+                            <Input id="contactName" />
+                        </Form.Item>
+                        <Form.Item
+                            label="Email"
+                            name="contactEmail"
+                            rules={[{
+                                required: true
+                                // message: 'Please input your username!' 
+                            }]}
+                        >
+                            <Input id="contactEmail" />
+                        </Form.Item>
+                        <Form.Item
+                            label="Subject"
+                            name="contactSubject"
+                            rules={[{
+                                // required: true
+                                // message: 'Please input your username!' 
+                            }]}
+                        >
+                            <Input id="contactSubject" />
+                        </Form.Item>
 
-                                <div>
-                                    <label htmlFor="contactEmail">
+                        <Form.Item
+                            label="Message"
+                            name="contactMessage"
+                            cols="50"
+                            rows="15"
+                            rules={[{
+                                required: true
+                                // message: 'Please input your username!' 
+                            }]}
+                        >
+                            <Input.TextArea id="contactMessage" />
+                        </Form.Item>
+                        <Button className="submit">Submit</Button>
+                    </Form>
 
-                                        Email <span className="required">*</span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        defaultValue=""
-                                        size="35"
-                                        id="contactEmail"
-                                        name="contactEmail"
-                                    />
-                                </div>
-
-                                <div>
-                                    <label htmlFor="contactSubject">Subject</label>
-                                    <input
-                                        type="text"
-                                        defaultValue=""
-                                        size="35"
-                                        id="contactSubject"
-                                        name="contactSubject"
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor="contactMessage">
-                                        Message <span className="required">*</span>
-                                    </label>
-                                    <textarea
-                                        cols="50"
-                                        rows="15"
-                                        id="contactMessage"
-                                        name="contactMessage"
-                                    ></textarea>
-                                </div>
-
-                                <div>
-                                    <button className="submit">Submit</button>
-                                    <span id="image-loader">
-                                        <img alt="" src="images/loader.gif" />
-                                    </span>
-                                </div>
-                            </fieldset>
-                        </form>
-
-                        <div id="message-warning"> Error boy</div>
-                        <div id="message-success">
-                            <i className="fa fa-check"></i>Your message was sent,
-                            thank you!
+                    <div id="message-warning"> Error boy</div>
+                    <div id="message-success">
+                        <i className="fa fa-check"></i>Your message was sent,
+                        thank you!
+                        <br />
+                    </div>
+                </Col>
+                <aside className="animate__animated animate__fadeInRight">
+                    <div className="">
+                        <h2>Address and Phone</h2>
+                        <p className="address">
+                            {name}
                             <br />
-                        </div>
-                    </Col>
-                    <aside className="animate__animated animate__fadeInRight">
-                        <div className="">
-                            <h2>Address and Phone</h2>
-                            <p className="address">
-                                {name}
-                                <br />
-                                {`${city}, ${country}`}
-                                <br />
-                                <span>{phone}</span>
-                            </p>
-                        </div>
-                    </aside>
-                </Row>
-            </Container>
+                            {`${city}, ${country}`}
+                            <br />
+                            <span>{phone}</span>
+                        </p>
+                    </div>
+                </aside>
+            </Row>
         </section>
     );
 }
